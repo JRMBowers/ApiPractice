456ace432f75bc6eb4d22ae339d1ad65eea63778
Bowers, Jamie (J.) <jbower34@ford.com>
2024-08-20 22:09:21 +0100

    create blank view model and test in order to follow TDD

---
+++ app/src/test/java/com/example/apipractice/LocationFinderViewModelTest.kt
@@ -0,0 +1,14 @@
+package com.example.apipractice
+
+import com.example.apipractice.viewModel.LocationFinderViewModel
+import org.junit.Test
+
+
+class LocationFinderViewModelTest {
+    val subject = LocationFinderViewModel()
+
+    @Test
+    fun `WHEN location code query is submitted THEN value returned`() {
+
+    }
+}
